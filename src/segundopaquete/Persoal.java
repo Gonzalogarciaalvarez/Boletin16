0e355b2491cd67daf25674717d2de63d5cdf4bf7
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2018-01-18 14:38:38 +0100

    Commit para subir

---
+++ src/segundopaquete/Persoal.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundopaquete;
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Persoal {
+    String telefono;
+    String correo;
+
+    public Persoal(String telefono, String correo) {
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return " telefono=" + telefono + ", correo=" + correo;
+    }
+    
+    
+    
+}
